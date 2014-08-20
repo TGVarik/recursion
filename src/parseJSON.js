5cf80ec4323102a821148039b99e91f79278b336
Tom G Varik <tgvarik@me.com>
2014-08-20 15:25:29 -0400

    Beginning work on parseJSON.

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,26 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+  var pointer;
+
+  var match = function(symbol){
+    if (symbol === json[pointer+1]){
+      pointer += 1;
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  var obj = function(){};
+  var members = function(){};
+  var pair = function(){};
+  var arr = function(){};
+  var elements = function(){};
+  var value = function(){};
+
+
+
 };
+
+
